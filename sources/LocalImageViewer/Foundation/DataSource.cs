aae97864bdd10792c69131687f387f0e92cb7981
[Ex] Yuki Tsuneyama <at12k313@gmail.com>
2023-01-04 03:53:46 +0900

    update loading style

--- sources/LocalImageViewer/Foundation/DataSource.cs
+++ sources/LocalImageViewer/Foundation/DataSource.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using YiSA.Foundation.Internal;
 namespace LocalImageViewer.Foundation
 {
     public class DataSourceUpdatedEventArgs<T> : EventArgs
@@ -31,8 +32,8 @@ namespace LocalImageViewer.Foundation
 
         private readonly List<T> _list = new();
         private readonly ConcurrentQueue<IEnumerable<T>> _queue = new();
-        private CancellationTokenSource _cancellationTokenSource = new ();
-        private readonly SlimLocker _locker = new ();
+        private CancellationTokenSource _cancellationTokenSource = new();
+        private readonly SlimLocker _locker = new();
         public bool IsLoading { get; private set; }
 
         public event EventHandler<DataSourceUpdatedEventArgs<T>> OnDataSourceManualAdded;
@@ -50,7 +51,7 @@ namespace LocalImageViewer.Foundation
                 return;
             }
 
-            IsLoading = true;
+            using var loading = StartLoading();
 
             _queue.TryDequeue(out var currentEnumerator);
             if (initial > 0)
@@ -95,9 +96,7 @@ namespace LocalImageViewer.Foundation
                     }
                 }
                 while (_queue.TryDequeue(out currentEnumerator));
-            },cancelToken);
-
-            IsLoading = false;
+            }, cancelToken);
         }
 
         public void Clear()
@@ -115,20 +114,26 @@ namespace LocalImageViewer.Foundation
             using var _ = _locker.WriteLock();
             int index = _list.Count - 1;
             _list.AddRange(items);
-            OnDataSourceManualAdded?.Invoke(this,new DataSourceUpdatedEventArgs<T>(index,items));
+            OnDataSourceManualAdded?.Invoke(this, new DataSourceUpdatedEventArgs<T>(index, items));
         }
 
         public void InsertRangeHead(params T[] items)
         {
             using var _ = _locker.WriteLock();
-            _list.InsertRange(0,items);
-            OnDataSourceManualAdded?.Invoke(this,new DataSourceUpdatedEventArgs<T>(0,items));
+            _list.InsertRange(0, items);
+            OnDataSourceManualAdded?.Invoke(this, new DataSourceUpdatedEventArgs<T>(0, items));
         }
 
         public void Dispose()
         {
             _cancellationTokenSource?.Cancel();
             _cancellationTokenSource?.Dispose();
+        }
+
+        private IDisposable StartLoading()
+        {
+            IsLoading = true;
+            return new DelegateDisposable(() => IsLoading = false);
         }
     }
 }
